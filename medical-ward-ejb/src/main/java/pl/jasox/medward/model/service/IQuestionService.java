827b063fdb95251825dd58484e6ab7e88870cfbe
Janusz Janowski <jasox@gazeta.pl>
2014-03-05 22:20:05 +0100

    services deleted

--- medical-ward-ejb/src/main/java/pl/jasox/medward/model/service/IQuestionService.java
+++
@@ -1,9 +0,0 @@
-package pl.jasox.medward.model.service;
-
-import java.util.List;
-
-import pl.jasox.medward.model.domainobject.MedProcedure;
-
-public interface IQuestionService {
-	List<MedProcedure> findAll();
-}
